5d7db8f2ab067f572586cf1477cd8f5b84a1b9c6
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2025-02-21 19:39:12 -0700

    primer commit

---
+++ src/prueba/Main.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola");
+
+	}
+
+}
